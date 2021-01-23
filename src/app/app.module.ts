76783beabf36cee99a1dae2ec5e910c14a293883
Srashti Jain <srashti0204@gmail.com>
2021-01-23 12:59:03 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentAComponent } from './components/component-a/component-a.component';
+import { ComponentBComponent } from './components/component-b/component-b.component';
+import { ComponentCComponent } from './components/component-c/component-c.component';
+import { ParentComponent } from './components/parent/parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentAComponent,
+    ComponentBComponent,
+    ComponentCComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule
